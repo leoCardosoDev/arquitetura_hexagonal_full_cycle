576b0d9e6a8e284c83bd6ee464b0dffae03a45c9
Leo Silva <leocardosodev@gmail.com>
2023-09-23 20:20:33 -0300

    feat: add Adapter to cli interface

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/leoCardosoDev/arquitetura_hexagonal_full_cycle/adapters/db"
-	"github.com/leoCardosoDev/arquitetura_hexagonal_full_cycle/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/leoCardosoDev/arquitetura_hexagonal_full_cycle/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Exemplo", 30)
-	productService.Enable(product)
+	cmd.Execute()
 }
